dcac378fd8c79e897257c84a565c6cc26cc330bc
Pablo Bernardo Baragaño <iam@pbernardo.com>
2023-04-27 20:34:22 +0200

    Telekasten mappings and highlights added

--- after/plugin/telekasten.lua
+++ after/plugin/telekasten.lua
@@ -1,5 +1,22 @@
+local home = vim.fn.expand("~/nsecondbrain") -- Put the name of your notes directory here
+
 require('telekasten').setup({
-  home = vim.fn.expand("~/nsecondbrain"), -- Put the name of your notes directory here
+	home = home,
+
+    -- if true, telekasten will be enabled when opening a note within the configured home
+    take_over_my_home = false,
+
+  -- template for new notes (new_note, follow_link)
+  -- set to `nil` or do not specify if you do not want a template
+  template_new_note = home .. '/' .. 'templates/new_note.md',
+
+  -- template for newly created daily notes (goto_today)
+  -- set to `nil` or do not specify if you do not want a template
+  template_new_daily = home .. '/' .. 'templates/daily.md',
+
+  -- template for newly created weekly notes (goto_thisweek)
+  -- set to `nil` or do not specify if you do not want a template
+  template_new_weekly= home .. '/' .. 'templates/weekly.md'
 })
 
 -- Launch panel if nothing is typed after <leader>z
@@ -18,3 +35,6 @@ vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
 -- Call insert link automatically when we start typing a link
 vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
 
+-- Color Highlights for Telekasten
+vim.cmd([[hi tkLink ctermfg=Blue cterm=bold,underline guifg=blue gui=bold,underline]])
+vim.cmd([[hi tkBrackets ctermfg=gray guifg=gray]])
