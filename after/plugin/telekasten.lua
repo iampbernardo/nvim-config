82719f3c55bd40dced01a975fcd8d119733f4568
Pablo Bernardo Baragaño <iam@pbernardo.com>
2023-04-27 22:41:18 +0200

    Keybinding added for telekasten

--- after/plugin/telekasten.lua
+++ after/plugin/telekasten.lua
@@ -24,6 +24,7 @@ vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
 
 -- Most used functions
 vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
+vim.keymap.set("n", "<leader>zp", "<cmd>Telekasten panel<CR>")
 vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
 vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
 vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
